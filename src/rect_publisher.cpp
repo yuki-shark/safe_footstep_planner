5373d199dbcb41af00725e5330f6a3d6803d99af
Yuki Omori <ohmori@jsk.imi.i.u-tokyo.ac.jp>
2018-11-02 22:00:56 +0900

    enable to set param from launch

--- src/rect_publisher.cpp
+++ src/rect_publisher.cpp
@@ -25,8 +25,8 @@ private:
 
 MaskImage::MaskImage() : nh_(""), private_nh_("~")
 {
-    rect_publisher_ = nh_.advertise<jsk_recognition_msgs::RectArray>("/mask_image_to_rect/output", 1);
-    camera_info_subscriber_ = nh_.subscribe("/zed/left/camera_info_raw", 1, &MaskImage::cameraInfoCallback, this);
+    rect_publisher_ = nh_.advertise<jsk_recognition_msgs::RectArray>("mask_image_to_rect/output", 1);
+    camera_info_subscriber_ = nh_.subscribe("zed/left/camera_info_raw", 1, &MaskImage::cameraInfoCallback, this);
 }
 
 
@@ -40,18 +40,18 @@ void MaskImage::rectPublisher(const std_msgs::Header header)
     jsk_recognition_msgs::RectArray msg;
     msg.header = header;
     jsk_recognition_msgs::Rect rect;
-    rect.x = 300;
-    rect.y = 100;
-    rect.width = 101;
-    rect.height = 201;
+    private_nh_.param("offset_x", rect.x,      0);
+    private_nh_.param("offset_y", rect.y,      0);
+    private_nh_.param("width",    rect.width,  1);
+    private_nh_.param("height",   rect.height, 1);
     msg.rects.push_back(rect) ;
     rect_publisher_.publish(msg);
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "mask_image");
-    MaskImage mask_image_node;
+    ros::init(argc, argv, "rect_publisher");
+    MaskImage rect_publisher_node;
     ros::spin();
 
     return 0;
