18e0484ae135510fe8049379c54e0997ca1972aa
Yuki Omori <ohmori@jsk.imi.i.u-tokyo.ac.jp>
2018-12-18 21:58:34 +0900

    set args and change some params for choreonoid

--- src/convert_to_map_coords.cpp
+++ src/convert_to_map_coords.cpp
@@ -21,6 +21,8 @@ class ImageConverter
     tf::TransformListener listener;
     ros::Subscriber info_sub_;
     sensor_msgs::CameraInfoConstPtr info_msg;
+    double original_x;
+    double original_y;
 
   public:
     ImageConverter()
@@ -37,6 +39,9 @@ class ImageConverter
 
     }
 
+    void setOriginalX(double x) { original_x = x; }
+    void setOriginalY(double y) { original_y = y; }
+
     void infoCb(const sensor_msgs::CameraInfoConstPtr& msg)
     {
         info_msg = msg;
@@ -47,7 +52,7 @@ class ImageConverter
         cv_bridge::CvImagePtr cv_ptr;
         try
         {
-            cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::TYPE_32SC1);
+            cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::MONO8);
         }
         catch (cv_bridge::Exception& e)
         {
@@ -63,8 +68,8 @@ class ImageConverter
             listener.lookupTransform("map", "static_virtual_camera", ros::Time(0), transform);
 
             tf::Vector3 original_coords;
-            original_coords.setValue(0.5, 0.2, 0.0);
-            std::cout << "original coords : " << "x : 0.5  y : 0.2  z : 0.0" << std::endl;
+            original_coords.setValue(original_x, original_y, 0.0);
+            std::cout << "original coords : " << "x : " << original_x << "  y : " << original_y << "  z : 0.0" << std::endl;
 
             tf::Vector3 translation_vector = transform.getOrigin();
             tf::Matrix3x3 rotation_matrix = transform.getBasis();
@@ -106,17 +111,19 @@ class ImageConverter
             std::cout << "radius : " << radius << std::endl;
             int left_u   = std::max(center_u - radius, 0);
             int right_u  = std::min(center_u + radius + 1, image_width);
-            int top_v    = std::max(center_u - radius, 0);
-            int bottom_v = std::min(center_u + radius + 1, image_width);
+            int top_v    = std::max(center_v - radius, 0);
+            int bottom_v = std::min(center_v + radius + 1, image_height);
 
-            int sum = 0;
-            for (int i=left_u; i<right_u; i++) {
-                for (int j=top_v; j<bottom_v; j++) {
-                    sum += cv_ptr->image.data[i*image_width + j];
+            int max_cost = 0;
+            for (size_t i=top_v; i<bottom_v; i++) {
+                for (size_t j=left_u; j<right_u; j++) {
+                    if (int(cv_ptr->image.data[i* image_width + j]) > max_cost) {
+                        max_cost = int(cv_ptr->image.data[i* image_width + j]);
+                    }
                 }
             }
 
-            double safety_cost = 1.0 * sum / ((right_u - left_u) * (bottom_v - top_v));
+            double safety_cost = double(max_cost);
 
             std::cout << "safety cost : " << safety_cost << std::endl;
             std::cout << "=============================================================" << std::endl;
@@ -124,7 +131,7 @@ class ImageConverter
         catch (tf::TransformException ex){
             ROS_ERROR("%s",ex.what());
             ros::Duration(1.0).sleep();
-        }
+       }
     }
 };
 
@@ -132,6 +139,8 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "image_converter");
     ImageConverter ic;
+    ic.setOriginalX(std::atof(argv[1]));
+    ic.setOriginalY(std::atof(argv[2]));
     ros::spin();
     return 0;
 }
