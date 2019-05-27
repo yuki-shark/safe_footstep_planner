f6cc6f9e957d7f95fff4ef118e0e76bce72af5e4
YutaKojio <kojio@jsk.imi.i.u-tokyo.ac.jp>
2019-05-27 13:47:17 +0900

    modify target height publisher

--- src/target_height_publisher.cpp
+++ src/target_height_publisher.cpp
@@ -7,7 +7,7 @@
 #include <pcl/conversions.h>
 #include <tf/transform_listener.h>
 #include <jsk_recognition_utils/pcl_conversion_util.h>
-#include <safe_footstep_planner/TargetFoothold.h>
+#include <safe_footstep_planner/OnlineFootStep.h>
 
 class TargetHeightPublisher
 {
@@ -17,7 +17,7 @@ public:
 
 private:
     void pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
-    void targetCallback(const safe_footstep_planner::TargetFoothold::ConstPtr& msg);
+    void targetCallback(const safe_footstep_planner::OnlineFootStep::ConstPtr& msg);
     ros::NodeHandle nh_;
     ros::NodeHandle pnh_;
     ros::Publisher height_publisher_;
@@ -29,9 +29,9 @@ private:
 
 TargetHeightPublisher::TargetHeightPublisher() : nh_(""), pnh_("~")
 {
-    height_publisher_ = nh_.advertise<geometry_msgs::PointStamped>("height", 1);
-    cloud_sub_ = nh_.subscribe("accumulated_heightmap_pointcloud/output", 10, &TargetHeightPublisher::pointcloudCallback, this);
-    target_sub_ = nh_.subscribe("target", 10, &TargetHeightPublisher::targetCallback, this);
+    height_publisher_ = nh_.advertise<safe_footstep_planner::OnlineFootStep>("landing_height", 1);
+    cloud_sub_ = nh_.subscribe("accumulated_heightmap_pointcloud_maprelative/output", 1, &TargetHeightPublisher::pointcloudCallback, this);
+    target_sub_ = nh_.subscribe("landing_target", 1, &TargetHeightPublisher::targetCallback, this);
 }
 
 void TargetHeightPublisher::pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
@@ -41,14 +41,15 @@ void TargetHeightPublisher::pointcloudCallback(const sensor_msgs::PointCloud2::C
     // std::cout << "cloud size : " << cloud_->size() << std::endl;
 }
 
-void TargetHeightPublisher::targetCallback(const safe_footstep_planner::TargetFoothold::ConstPtr& msg)
+void TargetHeightPublisher::targetCallback(const safe_footstep_planner::OnlineFootStep::ConstPtr& msg)
 {
-    double x = msg->target.x;
-    double y = msg->target.y;
+    double x = msg->x;
+    double y = msg->y;
+    double z = msg->z;
 
     // transform
     std::string target_frame;
-    if (msg->leg) {
+    if (msg->l_r) {
         target_frame = "/lleg_end_coords";
     }
     else {
@@ -56,42 +57,55 @@ void TargetHeightPublisher::targetCallback(const safe_footstep_planner::TargetFo
     }
 
     tf::StampedTransform transform;
-    listener_.lookupTransform("/BODY_on_map", target_frame, ros::Time(0), transform);
-    tf::Vector3 original_coords;
-    original_coords.setValue(x, y, 0.0);
+    listener_.lookupTransform("/map", target_frame, ros::Time(0), transform); // map relative to target_frame
+    tf::Vector3 landing_pos; // suppoting foot relative
+    landing_pos.setValue(x, y, z);
     tf::Vector3 translation_vector = transform.getOrigin();
     tf::Matrix3x3 rotation_matrix = transform.getBasis();
-    tf::Vector3 target_coords;
-    target_coords = rotation_matrix.inverse() * (original_coords - translation_vector);
+    tf::Vector3 rel_landing_pos; // map relative
+    rel_landing_pos = translation_vector + rotation_matrix * landing_pos;
+    std::cerr << "******************* target_frame : " << target_frame << std::endl;
+    std::cerr << "translation_vector: " << translation_vector.getX() << " , " << translation_vector.getY() << " , " << translation_vector.getZ() << std::endl;
 
-    // ROS_INFO("x: %f  y: %f  z: %f", target_coords.getX(), target_coords.getY(), target_coords.getZ());
-
-    double threshold = 0.01;
-    double az = 0.0;
-    int count = 0;
+    double threshold = 0.03;
+    double az = 0.0, az2 = 0.0;
+    int count = 0, count2 = 0;
     pcl::PointXYZ pp;
 
     if (cloud_) {
         for (int i = 0; i < cloud_->size(); i++) {
             pp = cloud_->points[i];
-            if (std::abs(pp.x - target_coords.getX()) < threshold &&
-                std::abs(pp.y - target_coords.getY()) < threshold) {
+            if (std::fabs(pp.x - rel_landing_pos.getX()) < threshold &&
+                std::fabs(pp.y - rel_landing_pos.getY()) < threshold) {
                 az += pp.z;
                 count++;
             }
+            if (std::fabs(pp.x - translation_vector.getX()) < threshold &&
+                std::fabs(pp.y - translation_vector.getY()) < threshold) {
+              az2 += pp.z;
+              count2++;
+            }
         }
-        // std::cout << "frame : " << target_frame << std::endl;
-        // std::cout << "z :  " << az / count + target_coords.getZ() << std::endl;
+        // ROS_INFO("x: %f  y: %f  z: %f,  rel_pos x: %f  rel_pos y: %f, az/count: %f", landing_pos.getX(), landing_pos.getY(), landing_pos.getZ(), rel_landing_pos.getX(), rel_landing_pos.getY(), az / count);
 
         // publish point
-        geometry_msgs::PointStamped ps;
+        safe_footstep_planner::OnlineFootStep ps;
         std_msgs::Header header;
         header.frame_id = target_frame.substr(1, target_frame.length() - 1);
         ps.header = header;
-        ps.point.x = x;
-        ps.point.y = y;
-        ps.point.z = az / count + target_coords.getZ();
+        tf::Vector3 tmp_pos;
+        rel_landing_pos.setZ(az / static_cast<double>(count));
+        translation_vector.setZ(az2 / static_cast<double>(count2));
+        tmp_pos = rotation_matrix.transpose() * (rel_landing_pos - translation_vector);
+        ps.x = tmp_pos.getX();
+        ps.y = tmp_pos.getY();
+        ps.z = tmp_pos.getZ();
+        ps.l_r = msg->l_r;
         height_publisher_.publish(ps);
+        std::cerr << "foot pos : "<< landing_pos.getX() << ", " << landing_pos.getY() << ", " << landing_pos.getZ() << std::endl;
+        std::cerr << "map pos swg: "<< rel_landing_pos.getX() << ", " << rel_landing_pos.getY() << ", " << rel_landing_pos.getZ() << std::endl;
+        std::cerr << "map pos sup: "<< translation_vector.getX() << ", " << translation_vector.getY() << ", " << translation_vector.getZ() << std::endl;
+        std::cerr << "height : "<< ps.x << ", " << ps.y << ", " << ps.z << ", " << ps.l_r << std::endl;
     }
 }
 
