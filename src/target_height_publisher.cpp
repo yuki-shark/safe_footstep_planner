7e645172a6d6b794b6c5b8401ed15207032ddb02
YutaKojio <kojio@jsk.imi.i.u-tokyo.ac.jp>
2019-05-30 22:44:23 +0900

    publish next landing rot

--- src/target_height_publisher.cpp
+++ src/target_height_publisher.cpp
@@ -21,6 +21,10 @@ public:
 private:
     void pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
     void targetCallback(const safe_footstep_planner::OnlineFootStep::ConstPtr& msg);
+    void matrixTFToEigen(const tf::Matrix3x3 &t, Eigen::Matrix3f &e);
+    void vectorTFToEigen(const tf::Vector3& t, Eigen::Vector3f& k);
+    void calcFootRotFromNormal (Eigen::Matrix3f& foot_rot, const Eigen::Matrix3f& orig_rot, const Eigen::Vector3f& n);
+    Eigen::Vector3f rpyFromRot(const Eigen::Matrix3f& m);
     ros::NodeHandle nh_;
     ros::NodeHandle pnh_;
     ros::Publisher height_publisher_;
@@ -61,34 +65,33 @@ void TargetHeightPublisher::targetCallback(const safe_footstep_planner::OnlineFo
 
     tf::StampedTransform transform;
     listener_.lookupTransform("/map", target_frame, ros::Time(0), transform); // map relative to target_frame
-    tf::Vector3 landing_pos; // suppoting foot relative
-    landing_pos.setValue(px, py, pz);
-    tf::Vector3 translation_vector = transform.getOrigin();
-    tf::Matrix3x3 rotation_matrix = transform.getBasis();
-    tf::Vector3 rel_landing_pos; // map relative
-    rel_landing_pos = translation_vector + rotation_matrix * landing_pos;
-    std::cerr << "******************* target_frame : " << target_frame << std::endl;
-    std::cerr << "translation_vector: " << translation_vector.getX() << " , " << translation_vector.getY() << " , " << translation_vector.getZ() << std::endl;
+    Eigen::Vector3f cur_foot_pos, ez(Eigen::Vector3f::UnitZ());
+    vectorTFToEigen(transform.getOrigin(), cur_foot_pos);
+    Eigen::Matrix3f tmp_cur_foot_rot, cur_foot_rot;
+    matrixTFToEigen(transform.getBasis(), tmp_cur_foot_rot);
+    calcFootRotFromNormal(cur_foot_rot, tmp_cur_foot_rot, ez);
+    Eigen::Vector3f next_foot_pos;
+    next_foot_pos = cur_foot_pos + cur_foot_rot * Eigen::Vector3f(px, py, pz);
 
-    double threshold = 0.03;
-    double az = 0.0, az2 = 0.0;
-    int count = 0, count2 = 0;
+    double threshold = 0.05;
+    double cur_az = 0.0, next_az = 0.0;
+    int count_cur = 0, count_next = 0;
     pcl::PointXYZ pp;
-    pcl::PointIndices::Ptr indices;
+    pcl::PointIndices::Ptr indices (new pcl::PointIndices);
 
     if (cloud_) {
         for (int i = 0; i < cloud_->size(); i++) {
             pp = cloud_->points[i];
-            if (std::fabs(pp.x - rel_landing_pos.getX()) < threshold &&
-                std::fabs(pp.y - rel_landing_pos.getY()) < threshold) {
-                az += pp.z;
-                count++;
+            if (std::fabs(pp.x - next_foot_pos(0)) < threshold &&
+                std::fabs(pp.y - next_foot_pos(1)) < threshold) {
+                next_az += pp.z;
+                count_next++;
                 indices->indices.push_back(i);
             }
-            if (std::fabs(pp.x - translation_vector.getX()) < threshold &&
-                std::fabs(pp.y - translation_vector.getY()) < threshold) {
-              az2 += pp.z;
-              count2++;
+            if (std::fabs(pp.x - cur_foot_pos(0)) < threshold &&
+                std::fabs(pp.y - cur_foot_pos(1)) < threshold) {
+              cur_az += pp.z;
+              count_cur++;
             }
         }
         // ROS_INFO("x: %f  y: %f  z: %f,  rel_pos x: %f  rel_pos y: %f, az/count: %f", landing_pos.getX(), landing_pos.getY(), landing_pos.getZ(), rel_landing_pos.getX(), rel_landing_pos.getY(), az / count);
@@ -98,19 +101,14 @@ void TargetHeightPublisher::targetCallback(const safe_footstep_planner::OnlineFo
         std_msgs::Header header;
         header.frame_id = target_frame.substr(1, target_frame.length() - 1);
         ps.header = header;
-        tf::Vector3 tmp_pos;
-        rel_landing_pos.setZ(az / static_cast<double>(count));
-        translation_vector.setZ(az2 / static_cast<double>(count2));
-        tmp_pos = rotation_matrix.transpose() * (rel_landing_pos - translation_vector);
-        ps.x = tmp_pos.getX();
-        ps.y = tmp_pos.getY();
-        ps.z = tmp_pos.getZ();
+        Eigen::Vector3f tmp_pos;
+        cur_foot_pos(2) = cur_az / static_cast<double>(count_cur);
+        next_foot_pos(2) = next_az / static_cast<double>(count_next);
+        tmp_pos = cur_foot_rot.transpose() * (next_foot_pos - cur_foot_pos);
+        ps.x = tmp_pos(0);
+        ps.y = tmp_pos(1);
+        ps.z = tmp_pos(2);
         ps.l_r = msg->l_r;
-        height_publisher_.publish(ps);
-        std::cerr << "foot pos : "<< landing_pos.getX() << ", " << landing_pos.getY() << ", " << landing_pos.getZ() << std::endl;
-        std::cerr << "map pos swg: "<< rel_landing_pos.getX() << ", " << rel_landing_pos.getY() << ", " << rel_landing_pos.getZ() << std::endl;
-        std::cerr << "map pos sup: "<< translation_vector.getX() << ", " << translation_vector.getY() << ", " << translation_vector.getZ() << std::endl;
-        std::cerr << "height : "<< ps.x << ", " << ps.y << ", " << ps.z << ", " << ps.l_r << std::endl;
 
         // estimage plane by RANSAC
         int minimun_indices = 10;
@@ -128,7 +126,6 @@ void TargetHeightPublisher::targetCallback(const safe_footstep_planner::OnlineFo
         seg.setMaxIterations(100);
         seg.segment(*inliers, *coefficients);
 
-        std::cerr << " inliers " << inliers->indices.size() << std::endl;
         if (inliers->indices.size() == 0) {
             std::cerr <<  " no plane" << std::endl;
         }
@@ -136,34 +133,83 @@ void TargetHeightPublisher::targetCallback(const safe_footstep_planner::OnlineFo
             std::cerr <<  " no enough inliners " << inliers->indices.size() <<  std::endl;
         }
         else {
-            Eigen::Vector3f z(0, 0, 1);
             jsk_recognition_utils::Plane plane(coefficients->values);
-            if (!plane.isSameDirection(z)) {
+              if (!plane.isSameDirection(ez)) {
                 plane = plane.flip();
             }
-            Eigen::Vector3f n = plane.getNormal();
-            // std::cerr << "x : " << n.getX() << "  y : " << n.getY() << "  z : " << n.getZ() << std::endl;
-            std::cerr << "plane normal : " << n << std::endl;
-            // Eigen::Vector3f x = pose_.matrix().block<3, 3>(0, 0) * Eigen::Vector3f::UnitX();
-            // if (acos(n.dot(x)) == 0) {
-            //     std::cerr << "hoge" << std::endl;
-            // }
-            // Eigen::Vector3f rotation_axis = n.cross(x).normalized();
-            // Eigen::Vector3f new_x = Eigen::AngleAxisf(M_PI / 2.0, rotation_axis) * n;
-            // if (acos(new_x.dot(x)) > M_PI / 2.0) {
-            //     new_x = - new_x;
-            // }
-            // Eigen::Vector3f new_y = n.cross(new_x);
-            // Eigen::Matrix3f new_rot_mat;
-            // new_rot_mat << new_x, new_y, n;
-            // Eigen::Quaternionf new_rot(new_rot_mat);
-            // Eigen::Vector3f p(pose_.translation());
-            // double alpha = (- plane.getD() - n.dot(p)) / (n.dot(z));
-            // Eigen::Vector3f q = p + alpha * z;
+            Eigen::Vector3f next_n = plane.getNormal();
+            next_n = cur_foot_rot.transpose() * next_n; // cur_foot relative
 
-            // Eigen::Affine3f new_pose = Eigen::Translation3f(q) * new_rot;
+            ps.nx =  next_n(0);
+            ps.ny =  next_n(1);
+            ps.nz =  next_n(2);
         }
+        height_publisher_.publish(ps);
     }
+}
+
+void TargetHeightPublisher::matrixTFToEigen(const tf::Matrix3x3 &t, Eigen::Matrix3f &e)
+{
+  for(int i=0; i<3; i++)
+    for(int j=0; j<3; j++)
+      e(i,j) = t[i][j];
+}
+
+void TargetHeightPublisher::vectorTFToEigen(const tf::Vector3& t, Eigen::Vector3f& k)
+{
+    k(0) = t[0];
+    k(1) = t[1];
+    k(2) = t[2];
+}
+
+Eigen::Vector3f TargetHeightPublisher::rpyFromRot(const Eigen::Matrix3f& m)
+{
+  double roll, pitch, yaw;
+
+  if ((fabs(m(0,0))<fabs(m(2,0))) && (fabs(m(1,0))<fabs(m(2,0)))) {
+    // cos(p) is nearly = 0
+    double sp = -m(2,0);
+    if (sp < -1.0) {
+      sp = -1;
+    } else if (sp > 1.0) {
+      sp = 1;
+    }
+    pitch = asin(sp); // -pi/2< p < pi/2
+
+    roll = atan2(sp*m(0,1)+m(1,2),  // -cp*cp*sr*cy
+                 sp*m(0,2)-m(1,1)); // -cp*cp*cr*cy
+
+    if (m(0,0)>0.0) { // cy > 0
+      (roll < 0.0) ? (roll += M_PI) : (roll -= M_PI);
+    }
+    double sr=sin(roll), cr=cos(roll);
+    if (sp > 0.0) {
+      yaw = atan2(sr*m(1,1)+cr*m(1,2), //sy*sp
+                  sr*m(0,1)+cr*m(0,2));//cy*sp
+    } else {
+      yaw = atan2(-sr*m(1,1)-cr*m(1,2),
+                  -sr*m(0,1)-cr*m(0,2));
+    }
+  } else {
+    yaw = atan2(m(1,0), m(0,0));
+    const double sa = sin(yaw);
+    const double ca = cos(yaw);
+    pitch = atan2(-m(2,0), ca*m(0,0)+sa*m(1,0));
+    roll = atan2(sa*m(0,2)-ca*m(1,2), -sa*m(0,1)+ca*m(1,1));
+  }
+  return Eigen::Vector3f(roll, pitch, yaw);
+}
+
+void TargetHeightPublisher::calcFootRotFromNormal (Eigen::Matrix3f& foot_rot, const Eigen::Matrix3f& orig_rot, const Eigen::Vector3f& n)
+{
+  Eigen::Vector3f ex = Eigen::Vector3f::UnitX();
+  Eigen::Vector3f xv1(orig_rot * ex);
+  xv1 = xv1 - xv1.dot(n) * n;
+  xv1.normalize();
+  Eigen::Vector3f yv1(n.cross(xv1));
+  foot_rot(0,0) = xv1(0); foot_rot(1,0) = xv1(1); foot_rot(2,0) = xv1(2);
+  foot_rot(0,1) = yv1(0); foot_rot(1,1) = yv1(1); foot_rot(2,1) = yv1(2);
+  foot_rot(0,2) = n(0);  foot_rot(1,2) = n(1);  foot_rot(2,2) = n(2);
 }
 
 int main(int argc, char **argv)
