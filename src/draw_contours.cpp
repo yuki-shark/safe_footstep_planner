acebfc26d65a6233671c17d95142637e06abc938
Yuki Omori <ohmori@jsk.imi.i.u-tokyo.ac.jp>
2019-09-30 23:21:29 +0900

    publish and image

--- src/draw_contours.cpp
+++ src/draw_contours.cpp
@@ -26,25 +26,25 @@ private:
   opencv_apps::ContourArrayStamped contours_;
   sensor_msgs::Image image_;
   ros::Publisher img_pub_;
+  ros::Publisher steppable_region_pub_;
   ros::Publisher msg_pub_;
 
   ros::Subscriber contours_sub_;
   ros::Subscriber board_hsv_sub_;
+  ros::Subscriber region_hsv_sub_;
 
   cv::Mat filled_convex_image_;
-  // ros::Publisher image_publisher_;
-  // void ContourArrayCallback(const opencv_apps::ContourArrayStamped::ConstPtr& msg);
   void ImageBoardCallback(const sensor_msgs::Image::ConstPtr& msg);
-  // void targetCallback(const safe_footstep_planner::OnlineFootStep::ConstPtr& msg);
+  void ImageRegionCallback(const sensor_msgs::Image::ConstPtr& msg);
 };
 
 DrawContours::DrawContours() : nh_(""), pnh_("~")
 {
   img_pub_ = nh_.advertise<sensor_msgs::Image>("board_image", 1);
+  steppable_region_pub_ = nh_.advertise<sensor_msgs::Image>("steppable_region_image", 1);
   msg_pub_ = nh_.advertise<opencv_apps::ContourArrayStamped>("hulls", 1);
-  // contours_sub_ = nh_.subscribe("/board_convex_full/hulls", 1, &DrawContours::ContourArrayCallback, this);
-  // board_hsv_sub_ = nh_.subscribe("/board_hsv_color_filter/image", 1, &DrawContours::ContourArrayCallback, this);
   board_hsv_sub_ = nh_.subscribe("/board_erode_mask_image2/output", 1, &DrawContours::ImageBoardCallback, this);
+  region_hsv_sub_ = nh_.subscribe("/left_erode_mask_image/output", 1, &DrawContours::ImageRegionCallback, this);
 }
 
 void DrawContours::ImageBoardCallback(const sensor_msgs::Image::ConstPtr& msg) {
@@ -54,8 +54,6 @@ void DrawContours::ImageBoardCallback(const sensor_msgs::Image::ConstPtr& msg) {
   std::vector<std::vector<cv::Point> > contours;
   std::vector<cv::Vec4i> hierarchy;
 
-  cv::RNG rng(12345);
-  // cv::Mat src_gray = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image;
   cv::Mat src_gray = cv_ptr->image;
 
   // Messages
@@ -74,14 +72,10 @@ void DrawContours::ImageBoardCallback(const sensor_msgs::Image::ConstPtr& msg) {
     cv::convexHull(cv::Mat(contours[i]), hull[i], false);
   }
 
-  // cv::Mat drawing = cv::Mat::zeros(src_gray.size(), CV_8UC3);
   cv::Mat drawing = cv::Mat::zeros(src_gray.size(), CV_8UC1);
   for (size_t i = 0; i < contours.size(); i++)
   {
-    // cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-    // cv::Scalar color = cv::Scalar(rng.uniform(0, 255));
     cv::Scalar color = cv::Scalar(255);
-    // cv::drawContours(drawing, contours, (int)i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
     cv::drawContours(drawing, hull, (int)i, color, -1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
 
     opencv_apps::Contour contour_msg;
@@ -96,9 +90,6 @@ void DrawContours::ImageBoardCallback(const sensor_msgs::Image::ConstPtr& msg) {
     contours_msg.contours.push_back(contour_msg);
   }
 
-  // Mat img_and;
-  // bitwise_and(img1, img2, img_and);
-
   filled_convex_image_ = drawing;
   sensor_msgs::Image::Ptr out_img =
     cv_bridge::CvImage(msg->header, sensor_msgs::image_encodings::MONO8, drawing).toImageMsg();
@@ -107,6 +98,24 @@ void DrawContours::ImageBoardCallback(const sensor_msgs::Image::ConstPtr& msg) {
 
 }
 
+void DrawContours::ImageRegionCallback(const sensor_msgs::Image::ConstPtr& msg) {
+  if (filled_convex_image_.empty()) {
+    return;
+  }
+  cv_bridge::CvImagePtr cv_ptr;
+  cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+
+  std::vector<std::vector<cv::Point> > contours;
+  std::vector<cv::Vec4i> hierarchy;
+
+  cv::Mat src_gray = cv_ptr->image;
+  cv::Mat img_and;
+  cv::bitwise_and(filled_convex_image_, src_gray, img_and);
+
+  sensor_msgs::Image::Ptr out_img =
+    cv_bridge::CvImage(msg->header, sensor_msgs::image_encodings::MONO8, img_and).toImageMsg();
+  steppable_region_pub_.publish(out_img);
+}
 
 int main(int argc, char **argv)
 {
